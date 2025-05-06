81cc4f55c0af933642b5df88b7e8988520f6c49d
Abdul Rahman <abdulrahman161004@gmail.com>
2025-05-06 15:38:33 +0530

    Created PropTypes and their default values

--- ReactProps/src/App.jsx
+++ ReactProps/src/App.jsx
@@ -3,11 +3,12 @@ import Car from './Car'
 export default function App()
 {
   const CarName = "BMW";
-  const CarInfo = { modal: "Mustank", price: "30000" };
+  const CarInfo = { modal: "Mustank", price: "30,000" };
 
   return (
     <>
-      <Car color="Yellow" brand={CarName} carInfo={CarInfo} />
+      <Car color="Yellow" brand={CarName} carInfo={CarInfo} month={"may"} date={6} />
+     
     </>
   )
 }
--- ReactProps/src/Car.jsx
+++ ReactProps/src/Car.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Car(props)
+function Car(
+    {
+        color,
+        brand,
+        carInfo,
+        date,
+        month
+
+    })
 {
     return (
         <div>
-            <h2>Car color : {props.color}</h2>
-            <h2>Car Brand: {props.brand}</h2>
-            <h2>Car Modal : {props.carInfo.modal}</h2>
-            <h2>Car Price : {props.carInfo.price}</h2>
+            <h2>Car color : {color}</h2>
+            <h2>Car Brand: {brand}</h2>
+            <h2>Car Modal : {carInfo.modal}</h2>
+            <h2>Car Price : {carInfo.price}</h2>
+            {/* Default */}
+            <h2>Car manufacturing Date: {date}</h2>
+            <h2>Car manufacturing month: {month}</h2>
         </div>
     )
 }
+
+// Define PropTypes for type checking
+Car.PropTypes = {
+    color: PropTypes.string,
+    brand: PropTypes.stirng,
+    carInfo: PropTypes.shape({
+        modal: PropTypes.string,
+        price: PropTypes.string
+    }),
+    date: PropTypes.number,
+    month: PropTypes.string
+
+}
+
+// Set default values for props
+Car.defaultProps = {
+    color: "Unknown",
+    brand: "Unknown",
+    carInfo: { modal: "Unknown", price: "N/A" },
+    date: 0,
+    month: "notSpecified"
+}
+
+
+
 export default Car;
