ccb0eb71be67c7bf9811aba612e987be1a2d2f8d
Abdul Rahman <abdulrahman161004@gmail.com>
2025-05-07 12:49:26 +0530

    feat: implemented deleteTask function to remove tasks by ID - Passed onDelete handler to Task and Tasks components

--- Task Tracker/src/App.jsx
+++ Task Tracker/src/App.jsx
@@ -35,13 +35,18 @@ export default function App()
     }
   ])
 
+  // Delete task
+  const deleteTask = (id) =>
+  {
+    setTasks(tasks.filter((task) => task.id != id))
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
-
 
 
