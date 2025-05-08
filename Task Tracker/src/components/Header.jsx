b5f5182f19e409b3dde13d78e02de806a529d81c
Abdul Rahman <abdulrahman161004@gmail.com>
2025-05-08 18:45:06 +0530

    feat: implement showAdd prop to toggle Add button text and color

--- Task Tracker/src/components/Header.jsx
+++ Task Tracker/src/components/Header.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title = " Task Tracker" }) =>
+const Header = ({ title = " Task Tracker", onAdd, showAdd }) =>
 {
-    const onClick = () =>
-    {
-        console.log("clicked")
-    }
     return (
         <div>
             <header className='header'>
                 <h1>{title}</h1>
-                <Button color='green' text='ADD' onClick={onClick} />
+                <Button
+                    color={showAdd ? 'red' : 'green'}
+                    text={showAdd ? 'Close' : 'Add'}
+                    onClick={onAdd}
+                />
             </header>
         </div>
     )
