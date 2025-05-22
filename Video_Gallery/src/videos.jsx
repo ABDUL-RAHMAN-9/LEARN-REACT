01831770d472a10bdce6985cc254221b66dc04c1
Abdul Rahman <abdulrahman161004@gmail.com>
2025-05-22 23:53:00 +0530

    feat: initialize custom video data component
    - Cleared default boilerplate (App.jsx, CSS)
    - Created ideo.jsx to define initial video data array
    - Each video object includes: id, videoId (Vimeo), videoName, previewImg
    - Data will be used to render video previews in UI later

---
+++ Video_Gallery/src/videos.jsx
@@ -0,0 +1,70 @@
+const videos = [
+    {
+        id: "1-0",
+        videoId: "315818543",
+        videoName: "Louis Vuitton - Tambour Horizon",
+        previewImg: "https://i.vimeocdn.com/video/2012789357-d408e7aa147733874603e5d39e8d49c1f4fbf511c9ffbd2a52c6b5ca5840ef0c-d_640x384?region=us"
+    },
+    {
+        id: "1-1",
+        videoId: "545748940",
+        videoName: "Ad Strategy Execution",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-3f593ca8-ce4f-4978-a9dd-772381131e68_2-1-4_2-1-4.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "1-2",
+        videoId: "675400031",
+        videoName: "ALAIA - SWIMWEAR WINTER-SPRING 2022",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-1be31408-49fb-41d1-a869-927c7671ef82_1-1-19_1-1-19.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "1-3",
+        videoId: "842826897",
+        videoName: "Mercedes CLE",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-ffdcd1be-bfec-401f-ace6-a94155f2335c_2-9-1_2-9-1.jpg?auto=compress,format&w=680&return"
+    },
+    {
+        id: "2-0",
+        videoId: "1013347946",
+        videoName: "Justin Bieber - Justic",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-8c8a563c-7259-4912-865d-2cb4743a2bff_justin_bieber_justice-2817-29.jpg?auto=compress,format&w=680&return"
+    },
+    {
+        id: "2-1",
+        videoId: "86294994",
+        videoName: "Showcase Trailer",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-43708a19-254c-46dc-82b8-e52cadb620f8_2ffda772-a5f3-44a5-ab7f-515b5b34c761.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "2-2",
+        videoId: "584150509",
+        videoName: "Brand Vision Promo",
+        previewImg: "https://images.unsplash.com/photo-1531811618757-cd2365d89cdc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "3-0",
+        videoId: "989783675",
+        videoName: "Minimal Flair Presentation",
+        previewImg: "https://images.unsplash.com/photo-1695634621375-0b66a9d5d1bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "3-1",
+        videoId: "569373486",
+        videoName: "Project Pulse Documentary",
+        previewImg: "https://images.unsplash.com/photo-1576714645198-7eebf227a483?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "3-2",
+        videoId: "437808118",
+        videoName: "Ad Creativity Concepts",
+        previewImg: "https://images.unsplash.com/photo-1606766121843-280da0b081fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA4fHxBZCUyMENyZWF0aXZpdHklMjBDb25jZXB0cyUyMHdpdGglMjBnaXJsfGVufDB8fDB8fHww"
+    },
+    {
+        id: "3-3",
+        videoId: "556442696",
+        videoName: "Soul Of The City",
+        previewImg: "https://images.unsplash.com/photo-1652710301785-3f10089fd61c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxTb3VsJTIwT2YlMjBUaGUlMjBDaXR5fGVufDB8fDB8fHww"
+    }
+];
+
+export default videos;
