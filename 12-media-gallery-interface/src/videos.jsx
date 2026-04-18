a449b8ca2bd5fc140b8e8f618e37aa7031dcee1d
Abdul Rahman <abdulrahman161004@gmail.com>
2026-04-18 15:24:44 +0530

    Refactor: [12-media-gallery-interface] - Built a cinematic gallery using linear interpolation (Lerp) for smooth parallax motion

---
+++ 12-media-gallery-interface/src/videos.jsx
@@ -0,0 +1,70 @@
+export const videos = [
+    {
+        id: "1-0",
+        videoId: "315818543",
+        videoName: "The Dauphine - Tambour Horizon",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-56bc21ab-2974-49ed-8676-24871fe32643_untitled_1-386-10.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "1-1",
+        videoId: "773257877",
+        videoName: "Ad Strategy Execution",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-3f593ca8-ce4f-4978-a9dd-772381131e68_2-1-4_2-1-4.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "1-2",
+        videoId: "632767757",
+        videoName: "A Life of Liberty - Teaser",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-01f5a752-1352-4788-a6c3-87c3849eda0b_1-1-1_1-1-1.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "1-3",
+        videoId: "842826897",
+        videoName: "Mercedes CLE",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-ffdcd1be-bfec-401f-ace6-a94155f2335c_2-9-1_2-9-1.jpg?auto=compress,format&w=680&return"
+    },
+    {
+        id: "2-0",
+        videoId: "1013347946",
+        videoName: "Justin Bieber - Justic",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-8c8a563c-7259-4912-865d-2cb4743a2bff_justin_bieber_justice-2817-29.jpg?auto=compress,format&w=680&return"
+    },
+    {
+        id: "2-1",
+        videoId: "800502145",
+        videoName: "Stefanie Soho - The Room",
+        previewImg: "https://www.keepgrading.com/_media/keepgrading-d040c22c-b3df-4730-8c0a-8060ef1d1fa1_img_1817.jpg?auto=compress,format&w=1440&return"
+    },
+    {
+        id: "2-2",
+        videoId: "584150509",
+        videoName: "Brand Vision Promo",
+        previewImg: "https://images.unsplash.com/photo-1531811618757-cd2365d89cdc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "3-0",
+        videoId: "989783675",
+        videoName: "Minimal Flair Presentation",
+        previewImg: "https://images.unsplash.com/photo-1695634621375-0b66a9d5d1bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "3-1",
+        videoId: "569373486",
+        videoName: "Project Pulse Documentary",
+        previewImg: "https://images.unsplash.com/photo-1576714645198-7eebf227a483?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "3-2",
+        videoId: "437808118",
+        videoName: "Ad Creativity Concepts",
+        previewImg: "https://images.unsplash.com/photo-1606766121843-280da0b081fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA4fHxBZCUyMENyZWF0aXZpdHklMjBDb25jZXB0cyUyMHdpdGglMjBnaXJsfGVufDB8fDB8fHww"
+    },
+    {
+        id: "3-3",
+        videoId: "556442696",
+        videoName: "Soul Of The City",
+        previewImg: "https://images.unsplash.com/photo-1652710301785-3f10089fd61c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxTb3VsJTIwT2YlMjBUaGUlMjBDaXR5fGVufDB8fDB8fHww"
+    }
+];
+
+export default videos;
